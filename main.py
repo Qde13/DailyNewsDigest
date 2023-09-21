7cc2ae2548c9abc2847af2233188c2bd8a1fe84d
Qde <nzemka0@gmail.com>
2023-09-21 17:26:32 +0300

    Access a titles and descriptions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 
 api_key = "e49117d391ec46d89bf0ccc2e297bebc"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-08-21&sortBy=publishedAt&apiKey=e49117d391ec46d89bf0ccc2e297bebc"
+url = ("https://newsapi.org/v2/everything?q=tesla" \
+       "&from=2023-08-21&sortBy=publishedAt&apiKey=" \
+       "e49117d391ec46d89bf0ccc2e297bebc")
 
 request = requests.get(url)
 
