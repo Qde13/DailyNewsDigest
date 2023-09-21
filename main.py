d104d540dd88a56d5470c22b4e90c6d3baeee5be
Qde <nzemka0@gmail.com>
2023-09-21 17:11:53 +0300

    Access a titles and descriptions

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+api_key = "e49117d391ec46d89bf0ccc2e297bebc"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2023-08-21&sortBy=publishedAt&apiKey=e49117d391ec46d89bf0ccc2e297bebc"
+
+request = requests.get(url)
+
+content = request.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
